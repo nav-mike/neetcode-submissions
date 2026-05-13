90c53557dfb4656cf3e9da76717091012955a5c6
Mikhail Navrotskii <navmike@protonmail.com>
2026-05-13 13:18:20 +0300

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+
+        for item in list(s):
+            if self.isOpen(item):
+                stack.append(item)
+            elif len(stack) == 0:
+                return False
+            elif self.isOpenFor(stack[-1], item):
+                stack.pop(-1)
+            else:
+                return False
+
+        return len(stack) == 0
+
+    def isOpen(self, s: str) -> bool:
+        return s == "[" or s == "{" or s == "("
+
+    def isOpenFor(self, s: str, f: str) -> bool:
+        return (s == "[" and f == "]") or (s == "{" and f == "}") or (s == "(" and f == ")")
